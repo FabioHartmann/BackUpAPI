9f6d562fce2b7acd1394951d184c11f955b4e73b
Felipe Machado Schultz <felipe.schultz@softdesign.com.br>
2019-05-28 13:33:26 -0300

    Alterações

--- src/controllers/backup-controller.ts
+++ src/controllers/backup-controller.ts
@@ -3,7 +3,7 @@ import * as request from 'request-promise-native'
 import download from 'image-downloader'
 
 import { CardClass } from './../class/Card-class'
-// import Card from './../models/card-model'
+import Card from './../models/card-model'
 
 const getData = async (url: string): Promise<CardClass[]> => {
   const body = await request.get(url)
@@ -11,78 +11,56 @@ const getData = async (url: string): Promise<CardClass[]> => {
   return response
 }
 
-const downloadImg = async (url: string, dest): Promise<void> => {
-  const options = {
-    url: url,
-    dest: dest
-  }
+const waitFor = (ms): Promise<void> => new Promise((resolve): any => setTimeout(resolve, ms))
 
-  return new Promise((resolve, reject): any => {
-    download.image(options)
-      .then((res): void => resolve(res))
-      .catch((err): void => reject(err))
+async function asyncForEach (array, callback): Promise<void> {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array)
+  }
+}
+const downloadImg = async (list, dest): Promise<void> => {
+  let cont = 0
+  await asyncForEach(list, async (link): Promise<void> => {
+    await waitFor(60)
+    await download.image({ url: link, dest: dest })
+      .then((res): void => console.log('Imagem', cont))
+      .catch((err): void => console.log(err))
+    cont += 1
   })
 }
 
-const downloadList = (list): boolean => {
+const insertData = async (list): Promise<void> => {
   let cont = 0
-
-  const interval = setInterval((): void => {
-    const card = list[cont]
-
-    if (cont < list.length) {
-      downloadImg(card.image_url, `${__dirname}/../img/pics`)
-      console.log(cont)
-    } else {
-      clearInterval(interval)
-    }
+  await asyncForEach(list, async (card): Promise<void> => {
+    await waitFor(60)
+    await Card.create(card)
+    console.log('Carta', cont)
     cont += 1
-  }, 200)
-  return true
+  })
 }
 
 class BackupController {
-  public async backup (req: Request, res: Response): Promise<Response> {
-    const resultData = await getData('https://db.ygoprodeck.com/api/v4/cardinfo.php?type=spell%20card')
+  public async backup (req: Request, res: Response): Promise<void> {
+    let resultData = await getData('https://db.ygoprodeck.com/api/v4/cardinfo.php?type=spell%20card&race=equip')
     console.log(resultData.length)
 
-    // resultData.forEach(async (card): Promise<void> => {
-    //   const urlImage = card.image_url
-    //   // const urlImageSmall = card.image_url_small
+    const normalImg = resultData.map((card): string => card.image_url)
+    const smallImg = resultData.map((card): string => card.image_url_small)
 
-    //   const normalImg = await downloadImg(urlImage, `${__dirname}/../img/pics`)
-    //   // const smallImg = await downloadImg(urlImageSmall, `${__dirname}/../img/small_pics`)
-    //   // const cardCreate = await Card.create(card)
-
-    //   console.log(normalImg)
-    //   // console.log(smallImg)
-    //   // console.log(cardCreate)
-    // })
-
-    downloadList(resultData)
-    return res.status(400).json({ success: true, msg: 'Backup concluido' })
+    downloadImg(normalImg, `${__dirname}/../img/pics`)
+      .then((): Promise<void> => {
+        return downloadImg(smallImg, `${__dirname}/../img/small_pics`)
+      })
+      .then((): Promise<void> => {
+        return insertData(resultData)
+      })
+      .then((): Response => {
+        return res.status(400).json({ success: false, msg: 'Backup concluido' })
+      })
+      .catch((): Response => {
+        return res.status(400).json({ success: false, msg: 'Erro no backup' })
+      })
   }
 }
 
 export default new BackupController()
-
-// https://db.ygoprodeck.com/api/v4/cardinfo.php?name=Dark%20Magician
-
-// download (url) {
-//   const options = {
-//     url: url,
-//     dest: `${__dirname}/img`
-//   }
-
-//   return new Promise((resolve, reject) => {
-//     download.image(options)
-//       .then((res) => {
-//         console.log(res)
-//         resolve(res)
-//       })
-//       .catch((err) => {
-//         console.log(err)
-//         reject(err)
-//       })
-//   })
-// }
